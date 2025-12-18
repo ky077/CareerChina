b1542e086368c3fc398aa8bfcf7704c9e9ba7a15
ky077 <kylin077@gmail.com>
2025-12-18 14:24:46 +0800

    D

--- senior/Placement/js/scrolltosection.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-	$(".btnToSection").click(function(event){
-		event.preventDefault();
-		$goSection = $(this).attr("href").slice(1);
-		$('html,body').animate({scrollTop:$('a[name='+$goSection+']').offset().top}, 800);
-	});
-});
--- senior/Placement/js/table-color.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	$(".tableTRColor tr").mouseover(function(){ $(this).addClass("over");})
-					  .mouseout(function(){ $(this).removeClass("over");}) 
-	
-	$(".tableTRColor tr:odd").addClass("oddalt"); //給的奇數行添加class值為oddalt
-	$(".tableTRColor tr:even").addClass("alt"); //給偶數行添加class值為alt
-		});
